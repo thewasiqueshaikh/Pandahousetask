e000e08ad1d6e5210777b791509b7974094ec949
unknown <skwasique98@gmail.com>
2021-05-27 19:17:40 +0530

    Task-1

---
+++ client/src/components/body/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+
+function Home() {
+    return (
+        <div className="home_page">
+            <h2>Hello Pandafuse!</h2>
+        </div>
+    )
+}
+
+export default Home
